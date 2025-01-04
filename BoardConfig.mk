openscine/device_xiaomi_topaz

topaz: Optimize native executables for Cortex-A73 CPU
Allow the compiler to take advantage of ARMv8a extensions to improve
performance in native executables, not just Java code compiled by ART.

Change-Id: I0e28dee8fb0e587dc0ceb742daa083d9cd16138d
Signed-off-by: Alexey Shelby <shelbyhell@proton.me>

BoardConfig.mk
@@ -36,7 +36,7 @@ BOARD_ANT_WIRELESS_DEVICE := "qualcomm-hidl"
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI := arm64-v8a
-TARGET_CPU_VARIANT := generic
+TARGET_CPU_VARIANT := cortex-a73
 
 # ART
 ART_BUILD_TARGET_NDEBUG := true

Nikita-Projects/android_device_xiaomi_topaz1

Signed-off-by: Nikita Savchenko <NikitaSavc@yandex.ru>

BoardConfig.mk
@@ -24,7 +24,7 @@ TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI := arm64-v8a
 TARGET_CPU_ABI2 := 
 TARGET_CPU_VARIANT := generic
-TARGET_CPU_VARIANT_RUNTIME := generic
+TARGET_CPU_VARIANT_RUNTIME := cortex-a73
 
 TARGET_2ND_ARCH := arm
 TARGET_2ND_ARCH_VARIANT := armv7-a-neon